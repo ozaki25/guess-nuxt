6c2cec497b7536acb05c0996e92fa3a4a2f32f9a
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-07 08:06:20 +0900

    hello

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/" exact>Home</nuxt-link>
+      <nuxt-link to="/example">Example</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+    <nuxt />
+  </div>
+</template>
